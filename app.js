c56e61048b00078c1604ec6effc1a5ccccf5b558
sarah smith01 <tooktun2547@gmail.com>
2022-10-08 21:38:29 +0700

    firstCommit

---
+++ app.js
@@ -0,0 +1,24 @@
+let adviceid = document.querySelector('#advice-id');
+let advicetext = document.querySelector('#advice-txt');
+let dice = document.querySelector('#dice')
+
+getAdvice()
+.then(()=>{
+    console.log("Success!");
+})
+.catch((err)=>{
+    console.log("Opps it's ERROR");
+    console.log(err);
+})
+
+async function getAdvice(){
+    const response = await fetch("https://api.adviceslip.com/advice",  { cache: "no-cache" });
+    const getdata = await response.json()
+    adviceid.innerHTML = getdata.slip.id
+    advicetext.innerHTML = getdata.slip.advice
+}
+getAdvice()
+
+dice.addEventListener("click", ()=>{
+    getAdvice()
+})
